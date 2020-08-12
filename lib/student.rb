a926ffc0d030d7942a1b5967d731d2436c8ca10a
Tyler Huffman <ty.huff24@gmail.com>
2020-08-12 17:46:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
+class Student < User
+attr_accessor :knowledge
 
+def initialize(first, last)
+  @first_name = first
+  @last_name = last
+  @knowledge = []
+end
+
+def learn(string)
+  @knowledge.push(string)
 end
